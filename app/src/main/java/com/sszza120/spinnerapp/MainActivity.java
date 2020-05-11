84d166d9ec980b86e65b856a94369e0b0a9767f0
sszza120 <z875ab@gmail.com>
2020-05-11 11:00:11 +0800

    完成專案

--- app/src/main/java/com/sszza120/spinnerapp/MainActivity.java
+++ app/src/main/java/com/sszza120/spinnerapp/MainActivity.java
@@ -2,12 +2,34 @@ package com.sszza120.spinnerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner sp_main, sp_vice;
+    private TextView txvshow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp_main = findViewById(R.id.sp_main);
+        sp_vice = findViewById(R.id.sp_vice);
+        txvshow = findViewById(R.id.txvshow);
+
+        String[] vice_meal_array = {"紅茶", "汽水", "果汁"};
+        ArrayAdapter<String> ad = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,vice_meal_array);
+
+        sp_vice.setAdapter(ad);
+    }
+
+    public void btnOK(View view) {
+        String main = sp_main.getSelectedItem().toString();
+        String vice = sp_vice.getSelectedItem().toString();
+
+        txvshow.setText("主餐:" + main + "\n" + "附餐:" + vice);
     }
 }
